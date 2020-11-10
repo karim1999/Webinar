5a94c14047141a02f12c356c17f459bc96b331c9
Karim Abdelgwad <karim.elbadry2@gmail.com>
2020-11-10 17:32:16 +0200

    https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,5 +26,8 @@ class AppServiceProvider extends ServiceProvider
     {
         //
 		Schema::defaultStringLength(191);
+		if (env('APP_ENV') !== 'local') {
+            $this->app['request']->server->set('HTTPS', true);
+        }
     }
 }
